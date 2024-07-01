17574e0251b512abc3ec174102b07500c32e24f9
саня <saneksrt222@gmail.com>
2024-07-02 02:47:06 +0400

    Пример блокировки

--- script.js
+++ script.js
@@ -20,8 +20,27 @@ let i = 0;
 
     c();
 
+    //блокировка в js
     console.log(1);
     for(let i = 0; i < 10e6; i++){
         performance.now();
     }
     console.log(2);
+
+    console.log(0);
+    setTimeout(() => {
+        console.log(2);
+    }, 0);
+    console.log(1);
+
+    setTimeout(() => {
+        console.log(1);
+    }, 1000);
+
+    setTimeout(() => {
+        console.log(2);
+    }, 2000);
+
+    setTimeout(() => {
+        console.log(3);
+    }, 3000);
